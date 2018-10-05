acc4586f70bfd3a50d776c9d4c745f7ed52e0a60
celfons <marcel.resende@yahoo.com.br>
2018-10-05 13:57:40 -0300

    added liquibase for migrations

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM postgres:9.6
-COPY database.sql /docker-entrypoint-initdb.d/db.sql
--- database.sql
+++
@@ -1,8 +0,0 @@
-create table public.message
-(
-  id    serial not null
-    constraint message_pkey
-    primary key,
-  name  text   not null,
-  msg text   not null
-);
